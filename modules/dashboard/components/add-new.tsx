ef14d6eeb44df2972e9701f656f8a75c92102462
ThakurShivam12 <shivamrudra2006@gmail.com>
2025-10-14 22:32:55 +0530

    template selecting modal

--- modules/dashboard/components/add-new.tsx
+++ modules/dashboard/components/add-new.tsx
@@ -7,6 +7,7 @@ import Image from "next/image"
 import { useRouter } from "next/navigation";
 import { useState } from "react"
 import { toast } from "sonner";
+import TemplateSelectingModal from "./template-selecting-modal";
 
 const AddNewButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -45,6 +46,11 @@ const AddNewButton = () => {
           />
         </div>
       </div>
+      <TemplateSelectingModal 
+        isOpen={isModalOpen}
+        onClose={()=>setIsModalOpen(false)}
+        onSubmit={()=>{}}
+      />
     </>
   )
 }
